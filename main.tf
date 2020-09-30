c8cbd8d3cf51c6349b19aaceedf10bc5f1feb025
Gabriel Nepomuceno <gabriel.bico@gmail.com>
2020-09-30 15:52:11 +0100

    Adding output

--- main.tf
+++ main.tf
@@ -175,6 +175,3 @@ PARAMETERS
   }
 }
 
-output "basic_set_id" {
-  value = azurerm_policy_set_definition.basic_set.id
-}
---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "basic_set_id" {
+  value = azurerm_policy_set_definition.basic_set.id
+}
