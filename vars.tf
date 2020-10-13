6ad328cc3ff3b8f54e6b7ecfc28edf7bef0768c9
greg oliver <golive@microsoft.com>
2020-10-13 12:28:28 +0100

    add management group to policy definition

--- vars.tf
+++ vars.tf
@@ -1,11 +1,11 @@
 variable "name" {
-    type = string
-    description = "Name of the initiative to be created"
-    default = "Auto Diagnostics Policy Initiative"
+  type        = string
+  description = "Name of the initiative to be created"
+  default     = "Auto Diagnostics Policy Initiative"
 }
 
 variable "management_group_name" {
-    type = string
-    description = "Mangment group that the policy should be created"
-    default = null
+  type        = string
+  description = "Mangment group that the policy should be created"
+  default     = null
 }
